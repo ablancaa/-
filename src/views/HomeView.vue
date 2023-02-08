987333723b27525dbfbc4e533638a27d8ebebb24
ablancaa <ablancaa@gmail.com>
2023-02-08 23:20:37 +0100

    Cambios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="home">
     <div id="contenido">
-    <!--<img alt="Vue logo" src="../assets/logo.png"> -->
-   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  
    <CardList :materiales="materiales"/>
   </div>
   </div>
