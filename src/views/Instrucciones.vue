280e5081ef905f2f000b4221f50c323745f8e80a
ablancaa <ablancaa@gmail.com>
2023-02-16 00:49:46 +0100

    Creo la página instrucciones para explicar el uso

---
+++ src/views/Instrucciones.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="container">
+    <h1 class="letraTitulo">Instrucciones</h1>
+        <div class="columns is-desktop">
+            <div class="column"><h1>1</h1><img src="../assets/logos/logo-VH-Master-RGB-blue.png"/></div>
+            <div class="column"><h1>2</h1><img src="../assets/logos/logo-VH-Master-RGB-blue.png"/></div>
+            <div class="column"><h1>3</h1><img src="../assets/logos/logo-VH-Master-RGB-blue.png"/></div>
+            <div class="column"><h1>3</h1><img src="../assets/logos/logo-VH-Master-RGB-blue.png"/></div>
+        </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Instrucciones-Vista",
+}
+</script>
+
+<style>
+.letraTitulo {
+  font-size: 35px;
+  margin-top: 0px;
+  margin-bottom: 10px;
+}
+</style>
