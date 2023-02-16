776a40d76999beacc90d2c27392d70f4e533a1a1
ablancaa <88708431+ablancaa@users.noreply.github.com>
2023-02-16 02:50:16 +0100

    Surface

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,7 +6,7 @@
       <img src="../assets/logos/logo-VH-Master-RGB-blue.png" width="112" height="28">
     </a> -->
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"> </span>
+      <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
